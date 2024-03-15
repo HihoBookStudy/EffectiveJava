947deb4b53d9ed91b5eaa5632903bea691254bd8
YJLEE <dldbwjd24@gmail.com>
2024-03-15 14:40:55 +0900

    clear git cache

---
+++ effectiveJava/src/main/java/javastudy/effectiveJava/EffectiveJavaApplication.java
@@ -0,0 +1,13 @@
+package javastudy.effectiveJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectiveJavaApplication.class, args);
+	}
+
+}
---
+++ effectiveJava/src/test/java/javastudy/effectiveJava/EffectiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package javastudy.effectiveJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
