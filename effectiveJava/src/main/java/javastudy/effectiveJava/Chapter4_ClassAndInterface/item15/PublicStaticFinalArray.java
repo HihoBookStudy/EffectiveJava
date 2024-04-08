d66e1b5730d0f51e6c9836d9f9bd48076c38f3a3
YujeongLEE <dldbwjd24@gmail.com>
2024-04-08 22:35:09 +0900

    item5 - 해결방안

--- effectiveJava/src/main/java/javastudy/effectiveJava/Chapter4_ClassAndInterface/item15/PublicStaticFinalArray.java
+++ effectiveJava/src/main/java/javastudy/effectiveJava/Chapter4_ClassAndInterface/item15/PublicStaticFinalArray.java
@@ -1,6 +1,8 @@
 package javastudy.effectiveJava.Chapter4_ClassAndInterface.item15;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class PublicStaticFinalArray {
 
@@ -21,9 +23,23 @@ public class PublicStaticFinalArray {
         System.out.println(Arrays.toString(fruits)); //[Lemon, Strawberry]
 
 
-        //두 가지 모두 같은 메모리주소를 가리킨다
+        //방법1. private array + unmodified
+
+        //변경시도 (add,remove,set..) 런타임 에러를 발생시킴
+        //FruitBasket.IMMUTABLE_PRIVATE_VALUES.set(0,new Fruit("Mango"));
+
+
+        //방법2. private array + 방어적 복사본 반환
+        Fruit[] fruits2 = FruitBasket.values();
+        fruits2[0] = new Fruit("Mango");
+        System.out.println(Arrays.toString(fruits2));//[Mango, Orange]
+
+
         System.out.println(FruitBasket.VALUES.toString()); //@7344699f
-        System.out.println(fruits.toString());//@7344699f
+        System.out.println(fruits.toString());//@7344699f (같은 객체임 -> 원본이 변경됨)
+        System.out.println(fruits2.toString());//@6b95977 (다른객체임)
+
+
     }
 
 }
@@ -45,10 +61,20 @@ class Fruit{
 
 class FruitBasket{
     public static final Fruit[] VALUES = {new Fruit("Banana"), new Fruit("Orange")};
+    private static final Fruit[] PRIVATE_VALUES = {new Fruit("Banana"), new Fruit("Orange")};
 
     public Fruit[] getValues(){
         return VALUES;
     }
 
+    //방법1. 불변리스트
+    public static final List<Fruit> IMMUTABLE_PRIVATE_VALUES =
+            Collections.unmodifiableList(Arrays.asList(PRIVATE_VALUES));
+
+
+    //방법2. 방어적복사
+    public static final Fruit[] values(){
+        return PRIVATE_VALUES.clone();
+    }
 
 }
