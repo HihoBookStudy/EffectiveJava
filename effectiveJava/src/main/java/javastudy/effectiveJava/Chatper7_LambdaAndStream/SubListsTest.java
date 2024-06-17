ebe73c06501261b37192e52197e3651545678604
YJLEE <dldbwjd24@gmail.com>
2024-06-17 16:26:05 +0900

    item47 - 입력리스트의 부분리스트

--- effectiveJava/src/main/java/javastudy/effectiveJava/Chatper7_LambdaAndStream/SubListsTest.java
+++ effectiveJava/src/main/java/javastudy/effectiveJava/Chatper7_LambdaAndStream/SubListsTest.java
@@ -1,5 +1,6 @@
 package javastudy.effectiveJava.Chatper7_LambdaAndStream;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -8,7 +9,7 @@ import java.util.stream.Stream;
 public class SubListsTest {
 
     public class SubLists{
-
+        
         public static <E> Stream<List<E>> of(List<E> list){
             return Stream.concat(Stream.of(Collections.emptyList()) // [] -> 빈리스트 하나 추가
                     //프리픽스의 서픽스
@@ -28,6 +29,29 @@ public class SubListsTest {
     }
 
 
+    public static <E> List<List<E>> makeListByLoop(List<E> list){
+
+        List<List<E>> result = new ArrayList<>(List.of(Collections.emptyList()));//빈리스트 기본
+
+        for(int start=0; start<list.size(); start++){
+            for(int end=start + 1; end <= list.size(); end++){
+                result.add(list.subList(start,end));
+            }
+        }
+        return result;
+    }
+
+
+    public static <E> Stream<List<E>> makeListByLoopUsingStream(List<E> list){
+
+        return IntStream.range(0, list.size())
+                .mapToObj(start ->
+                        IntStream.rangeClosed(start+1,list.size())
+                        .mapToObj(end -> list.subList(start,end)))//list로 생성한 subList를 Stream으로 모두 변환
+                .flatMap(x->x);
+
+    }
+
     public static void main(String[] args) {
 
 
@@ -37,7 +61,7 @@ public class SubListsTest {
         System.out.println("===========================================================");
         SubLists.prefixes(list).flatMap(SubLists::suffixes).forEach(System.out::println); // [a], / [a,b], [b] / [a,b,c], [b,c], [c]
 
-
-
+        System.out.println("FOR문 사용");
+        System.out.println(SubListsTest.makeListByLoop(list));
     }
 }
